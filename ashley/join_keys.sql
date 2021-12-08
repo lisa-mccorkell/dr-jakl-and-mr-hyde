cfaa3c7cca223a9f96d5a9611cec3e81eafa8bec
Ashley Lundeen <39098780+ashley-lundeen@users.noreply.github.com>
2021-12-08 11:51:14 -0800

    Create join_keys.sql

---
+++ ashley/join_keys.sql
@@ -0,0 +1,13 @@
+-- Farnsworth - MSSQL
+SELECT DISTINCT client AS client_name
+     , (CASE WHEN url IS NULL THEN '' ELSE url END) AS client_url
+     , sfdc_account_id AS account_id
+FROM Fulfillment.dbo.client
+
+
+
+-- HG360 - MSSQL
+SELECT name AS account_name
+     , (CASE WHEN website IS NULL THEN '' ELSE website END) AS account_url
+     , id AS account_id
+FROM HG360.dbo.Account
